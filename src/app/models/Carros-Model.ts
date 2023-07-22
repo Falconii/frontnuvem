577f751c0a0cd91f17025c9ef311283cf1a7b35f
Falconii <falconi.2050@gmail.com>
2023-07-22 12:53:24 -0300

    tentativa 3

---
+++ src/app/models/Carros-Model.ts
@@ -0,0 +1,10 @@
+export class CarrosModel {
+  public id_empresa: number = 0;
+  public placa: string = '';
+  public id_marca: number = 0;
+  public modelo: string = '';
+  public cor: string = '';
+  public ano: string = '';
+  public user_insert: number = 0;
+  public user_update: number = 0;
+}
---
+++ src/app/services/carros.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CarrosModel } from '../models/Carros-Model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CarrosServices {
+  apiURL: string = 'https://baknuvem-production.up.railway.app/api/';
+
+  constructor(private http: HttpClient) {}
+
+  getClientes(): Observable<CarrosModel[]> {
+    return this.http.get<CarrosModel[]>(`${this.apiURL}getcarros`);
+  }
+}
